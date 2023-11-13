d42fb8e1910b06ed7929fb456453619fca411f3c
Ma7moud72 <117377404+Ma7moud72@users.noreply.github.com>
2023-11-13 21:33:25 +0200

    Update taskTwo Programing.c

--- TaskOnePrograming.c
+++ TaskOnePrograming.c
@@ -1,91 +1,86 @@
-g#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_USERS 100
-
-typedef struct {
+struct User {
     char username[20];
     char password[20];
-} User;
+    union {
+        int flag;
+        char flag_string[4];
+    } flag;
+};
 
-User users[MAX_USERS];
-int totalUsers = 0;
+struct User users[10];
+int userCount = 0;
 
-void registerUser() {
-    if (totalUsers >= MAX_USERS) {
-        printf("Maximum number of users reached.\n");
-        return;
-    }
+void addUser(char *username, char *password);
+void registerUser();
+int login(char *username, char *password);
 
-    User newUser;
+int main() {
+    registerUser(); 
 
-    printf("Enter username: ");
-    scanf("%s", newUser.username);
-
-    // Check if the username already exists
-    for (int i = 0; i < totalUsers; i++) {
-        if (strcmp(users[i].username, newUser.username) == 0) {
-            printf("Username already exists. Please choose a different username.\n");
-            return;
-        }
-    }
-
-    printf("Enter password: ");
-    scanf("%s", newUser.password);
-
-    users[totalUsers] = newUser;
-    totalUsers++;
-
-    printf("Registration successful.\n");
-}
-
-void loginUser() {
     char username[20];
     char password[20];
 
-    printf("Enter username: ");
+    printf("please,Enter your username: ");
     scanf("%s", username);
 
     printf("Enter password: ");
     scanf("%s", password);
 
-    for (int i = 0; i < totalUsers; i++) {
-        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
-            printf("Login successful.\n");
-            return;
-        }
+    if (login(username, password) == 1) {
+        printf("Login successfully.\n");
+    } else {
+        printf("invalid username or password.\n");
     }
 
-    printf("Invalid username or password.\n");
+    return 0;
 }
 
-int main() {
-    int choice;
+void addUser(char *username, char *password) {
+    if (userCount >= 10) {
+        printf("Maximum user limit reached.\n");
+        return;
+    }
 
-    do {
-        printf("1. Register\n");
-        printf("2. Login\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+    strcpy(users[userCount].username, username);
+    strcpy(users[userCount].password, password);
+    userCount++;
+}
 
-        switch (choice) {
-            case 1:
-                registerUser();
-                break;
-            case 2:
-                loginUser();
-                break;
-            case 3:
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
+void registerUser() {
+    char username[20];
+    char password[20];
+    char flag_string[4];
+
+    printf("Enter a username for registration: ");
+    scanf("%s", username);
+
+    printf("Enter a password: ");
+    scanf("%s", password);
+
+    printf("Enter 'true' or '1' to activate the account, or 'false' or '0' to deactivate it: ");
+    scanf("%s", flag_string);
+
+    if (strcmp(flag_string, "true") == 0 || strcmp(flag_string, "1") == 0) {
+        users[userCount].flag.flag = 1;
+    } else if (strcmp(flag_string, "false") == 0 || strcmp(flag_string, "0") == 0) {
+        users[userCount].flag.flag = 0;
+    } else {
+        printf("Invalid flag value.\n");
+        return;
+    }
+
+    addUser(username, password);
+}
+
+int login(char *username, char *password) {
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0 && users[i].flag.flag == 1) {
+            return 1;
         }
-
-        printf("\n");
-    } while (choice != 3);
-
+    }
     return 0;
 }
